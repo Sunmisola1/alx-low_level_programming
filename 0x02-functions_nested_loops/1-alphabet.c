3d69a36fd60d7c2dafda940eadc6b3eb076fa425
Sunmisola1 <sunmisola012@gmail.com>
2023-03-04 14:00:58 -0800

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print all alphabet in lowercase
+ */
+
+void print_alphabet(void)
+{
+	char l;
+
+	for (l = 'a'; l <= 'z'; l++)
+	{
+		putchar(l);
+	}
+	purchar('\n')
+}
